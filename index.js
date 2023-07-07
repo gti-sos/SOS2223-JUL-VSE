466b79909b4c985999b054049094e97a25cf6760
vicsanesp <vse1997@gmail.com>
2023-07-07 11:42:37 +0200

    L05

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//hola
+var express = require("express");
+var cool = require("cool-ascii-faces");
+
+var app = express();
+var port = process.env.PORT || 12345;
+
+app.get("/faces", (request,response) => {
+    response.send(cool());
+    console.log("New Request");
+});
+
+app.listen(port, ()=>{
+    console.log(`Server ready in port ${port}`);
+});
