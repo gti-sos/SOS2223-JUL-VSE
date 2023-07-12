691fa9d95de67931f8df43e59fc17b07784d3231
vicsanesp <vse1997@gmail.com>
2023-07-12 12:16:26 +0200

    L13

--- frontend/src/routes/integrations/usosinproxy/+page.svelte
+++ frontend/src/routes/integrations/usosinproxy/+page.svelte
@@ -1,166 +1,69 @@
-<svelte:head>
-  <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-</svelte:head>
+<head>
+  <script src="https://code.highcharts.com/highcharts.js"></script>
+</head>
 
 <script>
-  //@ts-nocheck
-  import { onMount } from "svelte";
-  import { dev } from "$app/environment";
+  import { onMount } from 'svelte';
+  import Highcharts from 'highcharts';
+    import { redirect } from '@sveltejs/kit';
 
-
+  // @ts-ignore
+  let datos_vse = [];
+  let chart;
+  let API = 'https://sos2223-jul-vse.appspot.com/api/v1/agroprices-weekly/';
 
   onMount(async () => {
-    getDataComp();
+    await load_data();
+    draw_chart();
   });
 
-  let API_Comp = "https://sos2223-14.appspot.com/api/v2/apartment-occupancy-surveys";
-  
-  let API = "/api/v1/agroprices-weekly";
-  if (dev) API = "http://localhost:12345" + API;
-  let result = "";
-  let resultStatus = "";
-  const delay = d => new Promise(res => setTimeout(res, d));
-
-
-
-  async function getDataComp() {
-    const res = await fetch(API_Comp, {
-        method: 'GET'
-      });
+  async function load_data() {
     try {
-
-      const dataReceived = await res.json();
-      result = JSON.stringify(dataReceived, null, 2);
-      const data = dataReceived;
-      const status = await res.status;
-      resultStatus = status;
-
-    
-      let chartData_comp = data.map((i)=>{     
-        return {label:i.market, y:i.traveler};
-        });
-
-        console.log("comp:   "+JSON.stringify(chartData_comp, null, 2));
-
-      const chartData_unique = chartData_comp.filter((obj, index, arr) => {
-          return (
-            index === arr.findIndex((t) => {
-            return t.label === obj.label ;
-        })
-      );    });
-
-      console.log("unico : "+JSON.stringify(chartData_unique, null, 2));
-
-      let cmp = ['AL-Almería', 'CA-Cádiz', 'CO-Córdoba', 'GR-Granada', 'HU-Huelva', 'JA-Jaén', 'MA-Málaga', 'SE-Sevilla'];
-
-    
-      const chartData_ordered = chartData_unique.sort((a, b) => {    
-        return cmp.indexOf(a.label) - cmp.indexOf(b.label);
-
-    });
-
-      console.log("datoooooos ; "+JSON.stringify(chartData_ordered, null, 2));
-      getData(chartData_ordered);
+      const res = await fetch(API, { method: 'GET' });
+      const data = await res.json();
+      datos_vse = data;
     } catch (error) {
-      console.log(`Error fetching data: ${error}`);
+      console.log(`Error al cargar los datos: ${error}`);
     }
   }
 
-  async function getData(chartData_comp) {
-    const res = await fetch(API + "/data", {
-  
-        method: 'GET'
-      });
-    resultStatus = result = "";
-    try {
-      const dataReceived = await res.json();
-      const status = await res.status;
-      result = JSON.stringify(dataReceived, null, 2);
+  function draw_chart() {
+    // @ts-ignore
+    const seriesData = datos_vse.map(({ year, price, week}) =>
+      [year, price, week]
+    );
 
-      const data = dataReceived;
-      resultStatus = status;
-      await delay(500);
-      let chartData = [];
-      chartData = data;
+    const chartOptions = {
+      chart: {
+        type: 'area'
+      },
+      title: {
+        text: 'Gráfica de area | Highcharts'
+      },
+      xAxis: {
+        title: {
+          text: 'Año'
+        }
+      },
+      yAxis: {
+        title: {
+          text: 'Valor'
+        }
+      },
+      series: [{
+        name: 'Precio respecto a los años',
+        data: seriesData.map(([year, price]) => [year, price])
+      }, {
+        name: 'Precio respecto a las semanas',
+        data: seriesData.map(([week, , price]) => [week, price])
+      }]
+    };
 
-      loadCharts(chartData, chartData_comp);
-
-
-
-
-    } catch (error) {
-      console.log(`Error fetching data: ${error}`);
-    }
+    // @ts-ignore
+    chart = Highcharts.chart('chart-container', chartOptions);
   }
-
-  async function loadCharts(chartData, chartData_comp){
-
-    chartData = chartData.map(({market, products_number}) => ({
-        label: market,
-        y: Number(products_number) * 100000
-           
-
-   
-        }));
-    console.log(JSON.stringify(chartData_comp, null, 2));
-
-    //console.log(JSON.stringify(chartData, null, 2));
-
-
-
-        var chart = new CanvasJS.Chart("chartContainer", {
-            animationEnabled: true,
-            exportEnabled: true,	
-            title:{	
-                text: "PRODUCTOS FRENTE A TÚRISTAS EN ANDALUCÍA"             
-            }, 
-            axisY:{
-                title: "NÚMERO "
-            },
-            toolTip: {
-                shared: true
-            },
-            legend:{
-                cursor:"pointer",
-                itemclick: toggleDataSeries
-            },
-            data: [
-		    {        
-		
-                type: "spline",  
-                name: "Turístas",        
-                showInLegend: true,
-                dataPoints: chartData_comp
-            },  
-            {       
-                type: "spline",  	
-                name: "Productos",                
-                showInLegend: true,	
-                dataPoints: chartData
-	}]
-
-            
-});
-
-chart.render();
-
-function toggleDataSeries(e) {
-	if(typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-		e.dataSeries.visible = false;
-	}
-	else {
-		e.dataSeries.visible = true;            
-	}
-	chart.render();
-}
-
-}
-
-
-  
-
 </script>
 
-<body style="height: 70vh; padding: 20px; margin-top: 40px">
-  <div id="chartContainer" style="height: 300px; width: 100%;"></div>
-</body>
+<main>
+  <div id="chart-container"></div>
+</main>
